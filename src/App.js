45acc34b7e4d658c6e30201f85bf820c0001cb3a
Ivan <ivanchernetskey@mail.ru>
2021-02-20 14:58:46 +0300

    add style due to obj styles and added style{style.ul} for neccessary element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import React from 'react'
+import TodoList from './todo/TodoList'
 
 function App() {
   return (
-      <div className='wrapper' />
+      <div className='wrapper'>
+          <h1>React tutorial</h1>
+
+          <TodoList></TodoList>
+      </div>
   )
 }
 
---
+++ src/todo/TodoList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
+
+
+export default function TodoList() {
+    return(
+        //jsx code
+        <ul style={styles.ul}>
+            <li>1</li>
+            <li>2</li>
+        </ul>
+    )
+}
