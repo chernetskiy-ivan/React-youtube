ab67fe38b400d07d2cb14c2ef33b25286c85bb24
Ivan <ivanchernetskey@mail.ru>
2021-02-22 14:24:35 +0300

    hook useEffect(hook).Work with server

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import TodoList from './todo/TodoList'
 import Context from './context'
 import AddTodo from "./todo/AddTodo";
 
 function App() {
-    const [todos, setTodos] = React.useState([
-        {id: 1, completed: false, title: 'Купить хлеб'},
-        {id: 2, completed: false, title: 'Купить масло'},
-        {id: 3, completed: false, title: 'Купить молоко'}
-    ])
+    const [todos, setTodos] = React.useState([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos?_limit=5')
+            .then(response => response.json())
+            .then(todos => {
+                setTodos(todos)
+        })
+    }, [])
 
     function toggleTodo(id){
         setTodos(todos.map(todo => {
