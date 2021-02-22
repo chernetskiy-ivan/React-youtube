02d48378057679be2c6b0d925722eb7d6cee5b44
Ivan <ivanchernetskey@mail.ru>
2021-02-22 14:18:26 +0300

    created options for clear field after adding.we also use spread operator

--- src/todo/AddTodo.js
+++ src/todo/AddTodo.js
@@ -5,8 +5,12 @@ function useInputValue(defaultValue = '') {
     const [value,setValue] = useState(defaultValue)
 
     return {
-        value: value,
-        onChange: event => setValue(event.target.value)
+        bind:{
+            value: value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value
     }
 }
 
@@ -16,15 +20,15 @@ function AddTodo({onCreate}){
     function submitHandler(event){
         event.preventDefault()
 
-        if(input.value.trim()){
-            onCreate(input.value)
-            //setValue('')
+        if(input.value().trim()){
+            onCreate(input.value())
+            input.clear()
         }
     }
 
     return (
         <form style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
-            <input {...input} />
+            <input {...input.bind} />
             <button type={"submit"}>Add todo</button>
         </form>
     )
